6d61b8a52809c3f35b24d77d29e1c2b2d6fe4620
Henry <henry@Henry.local>
2017-10-08 15:37:09 +0800

    remove redundant

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,6 +19,7 @@ router.get("/", function(req, res){
 //CREATE - add new campground to DB
 router.post("/", middleware.isLoggedIn, function(req, res){
     // get data from form and add to campgrounds array
+    console.log('------------------')
     console.log(req)
     var name = req.body.name;
     var image = req.body.image;
