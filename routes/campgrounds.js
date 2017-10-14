77d2e7e9ed9bd2521033215b906b1ac60def1cae
matui <b98150@gmail.com>
2017-10-14 15:40:45 +0000

    Merge code

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -21,7 +21,6 @@ router.get("/", function(req, res){
 router.post("/", middleware.isLoggedIn, function(req, res){
     // get data from form and add to campgrounds array
     console.log('------------------')
-    console.log(req)
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
@@ -55,25 +54,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 
 //road - show form to create new campground
 router.get("/searchTraffic/:from/:to", function(req, res){
-<<<<<<< HEAD
-      let {from,to,type} = req.params
-      console.log(`Get traffic query from ${from} to ${to}`)
-      let result =[]
-      let miles = []
-      let current = undefined
-      let direction = undefined
-      let stations = []
-      let next = ''
-      console.log(JSON.stringify(interchanges[from]))
-      console.log(JSON.stringify(interchanges[to]))
-      for(var i in interchanges[from]){
-=======
-
-
-
-
-    let {etcs,interchanges} = require('../storage.js')
-    let {from,to,type} = req.params
+    let {from,to} = req.params
     console.log(`Get traffic query from ${from} to ${to}`)
     let result =[]
     let miles = []
@@ -81,122 +62,106 @@ router.get("/searchTraffic/:from/:to", function(req, res){
     let direction = undefined
     let stations = []
     let next = ''
-    console.log(JSON.stringify(interchanges[from]))
-    console.log(JSON.stringify(interchanges[to]))
-    
-
     for(var i in interchanges[from]){
->>>>>>> c00087236a68ccdf0d661b280f11aa7e5f153dcb
-	  let {mileage} = interchanges[from][i]
-	  direction = interchanges[from][i].direction
-	  for(var j in interchanges[to]){
-	      console.log(`A: ${JSON.stringify(interchanges[from][i])},B: ${JSON.stringify(interchanges[to][j])}`)
-	      if (direction == 'S'){
-		  if(mileage >= interchanges[to][j].mileage)
-		      continue
-	      } else {
-		  if(mileage <= interchanges[to][j].mileage)
-		      continue
-	      }
-              console.log(`Get ${interchanges[from][i]}`)
-              current = interchanges[from][i].id
-              break
-          }
-      if(current) break
+        let {mileage} = interchanges[from][i]
+	      direction = interchanges[from][i].direction
+	      for(var j in interchanges[to]){
+	          if (direction == 'S'){
+		            if(mileage >= interchanges[to][j].mileage)
+		                continue
+	          } else {
+		            if(mileage <= interchanges[to][j].mileage)
+		                continue
+	          }
+            current = interchanges[from][i].id
+            break
+        }
+        if(current) break
     }
-      do{
-          stations.push(etcs[current].From)
-          Object.keys(etcs[current].data).forEach((i)=>{
-          console.log(`Candidate : ${JSON.stringify(etcs[i])}`) 
-          if(etcs[i].Direction == direction && etcs[i].Road == etcs[current].Road)
-              next = i
-            })
-          console.log(etcs[current],next)
-          if(!next||stations.length > 30) break
-          let length = Number(Math.abs(etcs[current].Mileage - etcs[next].Mileage).toFixed(1))
-          result.push(etcs[current].data[next])
-          miles.push(length)
-          current = next
-      } while (current && etcs[current].To != to)
-      stations.push(to)
-      console.log(JSON.stringify(stations))
+    do{
+        stations.push(etcs[current].From)
+        Object.keys(etcs[current].data).forEach((i)=>{
+        if(etcs[i].Direction == direction && etcs[i].Road == etcs[current].Road)
+            next = i
+        })
+        if(!next||stations.length > 30) break
+        let length = Number(Math.abs(etcs[current].Mileage - etcs[next].Mileage).toFixed(1))
+        result.push(etcs[current].data[next])
+        miles.push(length)
+        current = next
+    } while (current && etcs[current].To != to)
+    stations.push(to)
+    console.log(JSON.stringify(stations))
 
 
-      let small_car = 0
-      let small_car_count = 0.0
-      let count1 = 0
-      let small_van = 0
-      let small_van_count = 0.0
-      let count2 = 0
-      let big_car = 0
-      let big_car_count = 0.0
-      let count3 = 0
-      let big_van = 0
-      let big_van_count = 0.0
-      let count4 = 0
-      let cuscate_van = 0
-      let cuscate_van_count = 0.0
-      let count5 = 0
-      let congestion = 1000000000
-      let congestion_place = ''
+    let small_car = 0
+    let small_car_count = 0.0
+    let count1 = 0
+    let small_van = 0
+    let small_van_count = 0.0
+    let count2 = 0
+    let big_car = 0
+    let big_car_count = 0.0
+    let count3 = 0
+    let big_van = 0
+    let big_van_count = 0.0
+    let count4 = 0
+    let cuscate_van = 0
+    let cuscate_van_count = 0.0
+    let count5 = 0
+    let congestion = 1000000000
+    let congestion_place = ''
 
-      console.log('==================')
-      for (var i=0; i<result.length; i++){
-        if (result[i]['31']['speed']!=='0'){
-          small_car += parseInt(result[i]['31']['speed'])
-          small_car_count +=1.0
-          count1 += parseInt(result[i]['31']['traffic'])
-          if (parseInt(result[i]['31']['speed']) < congestion ){
-            congestion = parseInt(result[i]['31']['speed'])
-            congestion_place = stations[i]
-          }
-        }
-        if (result[i]['32']['speed']!=='0'){
-          small_van += parseInt(result[i]['32']['speed'])
-          small_van_count +=1.0
-          count2 += parseInt(result[i]['32']['traffic'])
-        }
-        if (result[i]['41']['speed']!=='0'){
-          big_car += parseInt(result[i]['41']['speed'])
-          big_car_count +=1.0
-          count3 += parseInt(result[i]['41']['traffic'])
-        }
-        if (result[i]['42']['speed']!=='0'){
-          big_van += parseInt(result[i]['42']['speed'])
-          big_van_count +=1.0
-          count4 += parseInt(result[i]['42']['traffic'])
-        }
-        if (result[i]['5']['speed']!=='0'){
-          cuscate_van += parseInt(result[i]['5']['speed'])
-          cuscate_van_count +=1.0
-          count5 += parseInt(result[i]['5']['traffic'])
+    console.log('==================')
+    for (var i=0; i<result.length; i++){
+      if (result[i]['31']['speed']!=='0'){
+        small_car += parseInt(result[i]['31']['speed'])
+        small_car_count +=1.0
+        count1 += parseInt(result[i]['31']['traffic'])
+        if (parseInt(result[i]['31']['speed']) < congestion ){
+          congestion = parseInt(result[i]['31']['speed'])
+          congestion_place = stations[i]
         }
       }
-
-      console.log('|||||||||||||||||||')
-      avg_small_car = (small_car/small_car_count).toFixed(2)
-      avg_small_van = (small_van/small_van_count).toFixed(2)
-      avg_big_car = (big_car/big_car_count).toFixed(2)
-      avg_big_van = (big_van/big_van_count).toFixed(2)
-      avg_cuscate_van = (cuscate_van/cuscate_van_count).toFixed(2)
-
-      console.log("stations: " + stations.length)
-      console.log("miles: " + miles.length)
-      let sum_length = 0
-      for (var i=0;i<miles.length; i++){
-        sum_length += miles[i]
+      if (result[i]['32']['speed']!=='0'){
+        small_van += parseInt(result[i]['32']['speed'])
+        small_van_count +=1.0
+        count2 += parseInt(result[i]['32']['traffic'])
       }
+      if (result[i]['41']['speed']!=='0'){
+        big_car += parseInt(result[i]['41']['speed'])
+        big_car_count +=1.0
+        count3 += parseInt(result[i]['41']['traffic'])
+      }
+      if (result[i]['42']['speed']!=='0'){
+        big_van += parseInt(result[i]['42']['speed'])
+        big_van_count +=1.0
+        count4 += parseInt(result[i]['42']['traffic'])
+      }
+      if (result[i]['5']['speed']!=='0'){
+        cuscate_van += parseInt(result[i]['5']['speed'])
+        cuscate_van_count +=1.0
+        count5 += parseInt(result[i]['5']['traffic'])
+      }
+    }
 
-      sum_length = sum_length.toFixed(2)
-      
-      let time = ((sum_length / avg_small_car)*60).toFixed(2)
-      
+    console.log('|||||||||||||||||||')
+    avg_small_car = (small_car/small_car_count).toFixed(2)
+    avg_small_van = (small_van/small_van_count).toFixed(2)
+    avg_big_car = (big_car/big_car_count).toFixed(2)
+    avg_big_van = (big_van/big_van_count).toFixed(2)
+    avg_cuscate_van = (cuscate_van/cuscate_van_count).toFixed(2)
 
-      res.render('campgrounds/searchTraffic' ,{time: time, sum_length: sum_length, miles: miles, congestion_place: congestion_place, congestion: congestion, stations: stations, from: from, to: to, avg_small_car: avg_small_car,avg_small_van: avg_small_van,avg_big_car: avg_big_car, avg_big_van: avg_big_van, avg_cuscate_van: avg_cuscate_van, count1: count1, count2: count2, count3: count3, count4: count4, count5: count5});
-
-
-
-});
+    console.log("stations: " + stations.length)
+    console.log("miles: " + miles.length)
+    let sum_length = 0
+    for (var i=0;i<miles.length; i++){
+      sum_length += miles[i]
+    }
+    sum_length = sum_length.toFixed(2)
+    let time = ((sum_length / avg_small_car)*60).toFixed(2)
+    res.render('campgrounds/searchTraffic' ,{time: time, sum_length: sum_length, miles: miles, congestion_place: congestion_place, congestion: congestion, stations: stations, from: from, to: to, avg_small_car: avg_small_car,avg_small_van: avg_small_van,avg_big_car: avg_big_car, avg_big_van: avg_big_van, avg_cuscate_van: avg_cuscate_van, count1: count1, count2: count2, count3: count3, count4: count4, count5: count5});
+})
 
 //road - show form to create new campground
 router.get("/road", middleware.isLoggedIn, function(req, res){
@@ -219,12 +184,11 @@ router.get("/new", middleware.isLoggedIn, function(req, res){
   }
   Object.keys(interchanges).forEach((inter)=>{
     let i = interchanges[inter][0]
-    console.log(JSON.stringify(interchanges[inter]),rd)
     if (i.road.indexOf(rd)>-1){
       l1.push(inter)
     }
   })
-  res.render("campgrounds/new", {rd, l1}); 
+  res.render("campgrounds/new", {rd:req.query.road_type, l1}); 
 });
 
 // SHOW - shows more info about one campground
